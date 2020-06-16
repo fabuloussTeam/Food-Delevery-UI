37339ed7f0bcd8d9fa9880b77fdedb2496ce1be7
fabuloussTeam <tjomemmanuel@gmail.com>
2020-06-16 09:29:54 +0100

    Fin de la creation du UI du RecentOrder: Partie02

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -10,8 +10,73 @@ class RecentOrders extends StatelessWidget{
       margin: EdgeInsets.all(10.0),
       width: 320.0,
       decoration: new BoxDecoration(
-        color: Colors.white,
-        borderRadius: new BorderRadius.circular(15.0)
+          color: Colors.white,
+          borderRadius: new BorderRadius.circular(15.0),
+          border: Border.all(width: 1.0, color: Colors.grey[200])
+      ),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new Expanded(
+            child: new Row(
+              children: <Widget>[
+                new ClipRRect(
+                  borderRadius: new BorderRadius.circular(15.0),
+                  child: new Image(
+                    width: 100.0,
+                    height: 100.0,
+                    image: AssetImage(order.food.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                new Expanded(
+                  child: new Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        new SizedBox(height: 4.0),
+                        Text(
+                          order.restaurant.name,
+                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        new SizedBox(height: 4.0),
+                        Text(
+                          order.date,
+                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.0,
+            decoration: new BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                borderRadius: new BorderRadius.circular(30.0)
+            ),
+            child: new IconButton(
+              icon: new Icon(Icons.add),
+              color: Colors.white,
+              iconSize: 30.0,
+              onPressed: (){},
+            ),
+
+          )
+        ],
       ),
     );
   }
@@ -35,13 +100,15 @@ class RecentOrders extends StatelessWidget{
         ),
         new Container(
           height: 120.0,
-          color: Colors.blue,
+          // color: Colors.blue,
           child: new ListView.builder(
-            scrollDirection: Axis.horizontal,
+              physics: new BouncingScrollPhysics(), // pour tirrer l'image en fin ligne
+              padding: EdgeInsets.only(left: 10.0),
+              scrollDirection: Axis.horizontal, // Organise les elements de la listeView de facon horizontal
               itemCount: currentUser.orders.length,
               itemBuilder: (BuildContext context, int index){
-                 Order order = currentUser.orders[index];
-                 return _buildRecentOrder(context, order);
+                Order order = currentUser.orders[index];
+                return _buildRecentOrder(context, order);
               }
           ),
         )
