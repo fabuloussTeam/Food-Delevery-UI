b30a47682c43a38b5e64ed64607e8dbde14be058
fabuloussTeam <tjomemmanuel@gmail.com>
2020-06-19 11:01:29 +0100

    Affichage des menu d'un restaurant: GridView menu

--- lib/screen/restaurant_screen.dart
+++ lib/screen/restaurant_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/food.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_start.dart';
 
@@ -15,48 +17,133 @@ class RestaurantScreen extends StatefulWidget{
 
 class _RestaurantScreenState extends State<RestaurantScreen>{
 
+  _buildMenuItem(Food menuItem, double height, double width){
+    return new Center(
+      child: new Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+         new Container(
+           width:  width / 2.5,
+           height: width / 2.5,
+           decoration: new BoxDecoration(
+             image: DecorationImage(
+               image: AssetImage(menuItem.imageUrl),
+               fit: BoxFit.cover,
+             ),
+             borderRadius: new BorderRadius.circular(15.0)
+           ),
+          ),
+          new Container(
+            width:  width / 2.5,
+            height: width / 2.5,
+            decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.circular(15.0),
+              gradient: new LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.black.withOpacity(0.3),
+                    Colors.black87.withOpacity(0.3),
+                    Colors.black54.withOpacity(0.3),
+                    Colors.black38.withOpacity(0.3),
+                  ],
+                stops: [0.1, 0.4, 0.6, 0.9]
+              ),
+            ),
+          ),
+          new Positioned(
+            bottom: 65.0,
+            child: new Column(
+              children: <Widget>[
+                new Text(
+                  menuItem.name,
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2
+                  ),
+                ),
+                new Text(
+                  "\$${menuItem.price.toString()}",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2
+                  ),
+                )
+              ],
+            ),
+          ),
+          new Positioned(
+            bottom: 10.0,
+            right: 5.0,
+            child: new Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.00,
+            decoration: new BoxDecoration(
+               borderRadius: new BorderRadius.circular(30.0),
+               color: Colors.deepOrangeAccent
+            ),
+              child:  new IconButton(
+                  icon: Icon(Icons.add),
+                  color: Colors.white,
+                  iconSize: 30.0,
+                  onPressed: () => Navigator.pop(context)
+              ),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     double heigth = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
     return new Scaffold(
-      body: new Column(
-        children: <Widget>[
-          new Stack(
-            children: <Widget>[
-              new Hero(
-                tag: widget.restaurant.imageUrl,
-                child: new Image(
-                  image: AssetImage(widget.restaurant.imageUrl),
-                  height: heigth / 3.0,
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.cover,
+      body: new SingleChildScrollView(
+        child: new Column(
+          children: <Widget>[
+            new Stack(
+              children: <Widget>[
+                new Hero(
+                  tag: widget.restaurant.imageUrl,
+                  child: new Image(
+                    image: AssetImage(widget.restaurant.imageUrl),
+                    height: heigth / 3.0,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              new Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: heigth / 14),
-                child: new  Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new IconButton(
-                        icon: Icon(Icons.arrow_back_ios),
-                        color: Colors.white,
-                        iconSize: 30.0,
-                        onPressed: () => Navigator.pop(context)
-                    ),
-                    new IconButton(
-                      icon: Icon(Icons.favorite),
-                      color: Colors.deepOrangeAccent,
-                      iconSize: 35.0,
-                      onPressed: () => {},
-                    )
-                  ],
+                new Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: heigth / 14),
+                  child: new  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new IconButton(
+                          icon: Icon(Icons.arrow_back_ios),
+                          color: Colors.white,
+                          iconSize: 30.0,
+                          onPressed: () => Navigator.pop(context)
+                      ),
+                      new IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: Colors.deepOrangeAccent,
+                        iconSize: 35.0,
+                        onPressed: () => {},
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          new Padding(
+              ],
+            ),
+            new Padding(
               padding: EdgeInsets.all(20.0),
               child: new  Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,18 +174,18 @@ class _RestaurantScreenState extends State<RestaurantScreen>{
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       new FlatButton(
-                          onPressed: (){},
+                        onPressed: (){},
                         color: Colors.deepOrangeAccent,
                         shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0)
+                            borderRadius: new BorderRadius.circular(20.0)
                         ),
-                          child: new Text(
-                            "Reviews",
-                            style: new TextStyle(
+                        child: new Text(
+                          "Reviews",
+                          style: new TextStyle(
                               color: Colors.white,
                               fontSize: 20.0
-                            ),
                           ),
+                        ),
                       ),
                       new FlatButton(
                         onPressed: (){},
@@ -116,12 +203,43 @@ class _RestaurantScreenState extends State<RestaurantScreen>{
                       )
                     ],
                   ),
+                  new SizedBox(height: 10.0),
+                  new Center(
+                    child: new Text(
+                      "Menu",
+                      style: new TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.2
+                      ),
+                    ),
+                  ),
+                  new SizedBox(height: 10.0),
+                  new Container(
+                   height: heigth / 2,
+                  /*  decoration: new BoxDecoration(
+                        color: Colors.red
+                    ),*/
+                    child: new GridView.count(
+                      padding: EdgeInsets.all(10.0),
+                      crossAxisCount: 2,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: List.generate(widget.restaurant.menu.length, (i) {
+                        Food food = widget.restaurant.menu[i];
+                        print(food.name);
+                        return new Card(
+                          child: _buildMenuItem(food, heigth, width)
+                        );
+                      }),
+                    ),
+                  )
 
                 ],
               ),
-
-          ),
-        ],
+            ),
+          ],
+        ),
       )
     );
   }
